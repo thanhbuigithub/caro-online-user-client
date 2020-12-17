226bcdc0e1c24d37426f84fb67043d33b079ef91
Thanh. Bui Chi <thanhbuint123@gmail.com>
2020-12-17 13:57:22 +0700

    change repo from caro-online-web-nang-cao to caro-online-user-client

---
+++ src/components/game/index.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { useState, useContext } from "react";
+// import "./index.css";
+import Board from "./board";
+import Grid from "@material-ui/core/grid";
+import { makeStyles } from "@material-ui/core/styles";
+import GameContext from "../../contexts/GameContext";
+import SocketManager from "../../socketio/SocketManager";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+function Game({ match }) {
+  const classes = useStyles();
+  const { setRoomId, handleClick } = useContext(GameContext);
+  const socket = SocketManager.getSocket();
+
+  useEffect(() => {
+    setRoomId(match.params.id);
+    console.log("RESET GAME");
+
+    socket.on("new-player-join-room", (username) => {
+      console.log(username + " has joined");
+    });
+
+    // socket.on("move", ({ row, col }) => {
+    //   console.log(`Socket: on: move ${row} ${col}`);
+    //   handleClick(row, col);
+    // });
+
+    return () => {
+      socket.off();
+    };
+  }, []);
+
+  return (
+    <div className="game">
+      <Grid container className={classes.root}>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <Grid item xs={2} justify="center">
+              User
+            </Grid>
+            <Grid item xs={8} justify="center">
+              <Board />
+            </Grid>
+            <Grid item xs={2} justify="center">
+              Chat
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/provider/GameProvider.js
@@ -0,0 +1,249 @@
+import React from "react";
+import { useState } from "react";
+import GameContext from "../contexts/GameContext";
+import config from "../config/Config";
+
+const GameProvider = (props) => {
+  const [history, setHistory] = useState([
+    {
+      x: null,
+      y: null,
+      cells: Array(config.boardSize.row)
+        .fill(null)
+        .map(() => {
+          return Array(config.boardSize.col).fill(null);
+        }),
+    },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [turn, setTurn] = useState(config.playerX);
+  const [isAscending, setIsAscending] = useState(true);
+  const [gameover, setGameover] = useState(false);
+  const [winCells, setWinCells] = useState(null);
+  const [isWaiting, setIsWaiting] = useState(true);
+  const [roomInfo, setRoomInfo] = useState({});
+  const [playerType, setPlayerType] = useState(config.playerX);
+  const [roomId, setRoomId] = useState("");
+  const [chatHistory, setChatHistory] = useState({});
+
+  function handleClick(row, col) {
+    console.log("GameProvider: handleClick");
+    const curHistory = history.slice(0, stepNumber + 1);
+    const current = curHistory[curHistory.length - 1];
+    const cells = JSON.parse(JSON.stringify(current.cells));
+
+    if (gameover || cells[row][col]) {
+      return false;
+    }
+
+    cells[row][col] = turn;
+
+    let _winCells = checkWin(row, col, turn, curHistory.length - 1);
+
+    if (_winCells !== null) {
+      setWinCells(_winCells);
+      setGameover(true);
+    }
+
+    console.log("setHistory: ");
+
+    setHistory((history) => {
+      const curHistory = history.slice(0, stepNumber + 1);
+      return curHistory.concat([
+        {
+          cells: cells,
+          x: row,
+          y: col,
+          turn: turn,
+        },
+      ]);
+    });
+
+    setStepNumber((stepNumber) => {
+      return curHistory.length;
+    });
+    console.log(curHistory);
+    console.log("turn: " + turn);
+    let nextTurn = turn === config.playerX ? config.playerO : config.playerX;
+    console.log("next turn: " + nextTurn);
+    setTurn((turn) =>
+      turn === config.playerX ? config.playerO : config.playerX
+    );
+
+    return true;
+  }
+
+  function checkWin(row, col, user, stepNumber) {
+    if (stepNumber === 0) {
+      return null;
+    }
+
+    const current = history[stepNumber];
+    const cells = current.cells.slice();
+
+    // Get coordinates
+    let coorX = row;
+    let coorY = col;
+
+    let countCol = 1;
+    let countRow = 1;
+    let countMainDiagonal = 1;
+    let countSkewDiagonal = 1;
+    let isBlock;
+    const rival = user === config.xPlayer ? config.oPlayer : config.xPlayer;
+
+    // Check col
+    isBlock = true;
+    let winCells = [];
+    coorX -= 1;
+    while (coorX >= 0 && cells[coorX][coorY] === user) {
+      countCol += 1;
+      winCells.push([coorX, coorY]);
+      coorX -= 1;
+    }
+    if (coorX >= 0 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorX = row;
+    winCells.push([coorX, coorY]);
+    coorX += 1;
+    while (coorX <= config.brdSize - 1 && cells[coorX][coorY] === user) {
+      countCol += 1;
+      winCells.push([coorX, coorY]);
+      coorX += 1;
+    }
+    if (coorX <= config.brdSize - 1 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorX = row;
+    if (isBlock === false && countCol >= 5) return winCells;
+
+    // Check row
+    isBlock = true;
+    winCells = [];
+    coorY -= 1;
+    while (coorY >= 0 && cells[coorX][coorY] === user) {
+      countRow += 1;
+      winCells.push([coorX, coorY]);
+      coorY -= 1;
+    }
+    if (coorY >= 0 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorY = col;
+    winCells.push([coorX, coorY]);
+    coorY += 1;
+    while (coorY <= config.brdSize - 1 && cells[coorX][coorY] === user) {
+      countRow += 1;
+      winCells.push([coorX, coorY]);
+      coorY += 1;
+    }
+    if (coorY <= config.brdSize - 1 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorY = col;
+    if (isBlock === false && countRow >= 5) return winCells;
+
+    // Check main diagonal
+    isBlock = true;
+    winCells = [];
+    coorX -= 1;
+    coorY -= 1;
+    while (coorX >= 0 && coorY >= 0 && cells[coorX][coorY] === user) {
+      countMainDiagonal += 1;
+      winCells.push([coorX, coorY]);
+      coorX -= 1;
+      coorY -= 1;
+    }
+    if (coorX >= 0 && coorY >= 0 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorX = row;
+    coorY = col;
+    winCells.push([coorX, coorY]);
+    coorX += 1;
+    coorY += 1;
+    while (
+      coorX <= config.brdSize - 1 &&
+      coorY <= config.brdSize - 1 &&
+      cells[coorX][coorY] === user
+    ) {
+      countMainDiagonal += 1;
+      winCells.push([coorX, coorY]);
+      coorX += 1;
+      coorY += 1;
+    }
+    if (
+      coorX <= config.brdSize - 1 &&
+      coorY <= config.brdSize - 1 &&
+      cells[coorX][coorY] !== rival
+    ) {
+      isBlock = false;
+    }
+    coorX = row;
+    coorY = col;
+    if (isBlock === false && countMainDiagonal >= 5) return winCells;
+
+    // Check skew diagonal
+    isBlock = true;
+    winCells = [];
+    coorX -= 1;
+    coorY += 1;
+    while (coorX >= 0 && coorY >= 0 && cells[coorX][coorY] === user) {
+      countSkewDiagonal += 1;
+      winCells.push([coorX, coorY]);
+      coorX -= 1;
+      coorY += 1;
+    }
+    if (coorX >= 0 && coorY >= 0 && cells[coorX][coorY] !== rival) {
+      isBlock = false;
+    }
+    coorX = row;
+    coorY = col;
+    winCells.push([coorX, coorY]);
+    coorX += 1;
+    coorY -= 1;
+    while (
+      coorX <= config.brdSize - 1 &&
+      coorY <= config.brdSize - 1 &&
+      cells[coorX][coorY] === user
+    ) {
+      countSkewDiagonal += 1;
+      winCells.push([coorX, coorY]);
+      coorX += 1;
+      coorY -= 1;
+    }
+    if (
+      coorX <= config.brdSize - 1 &&
+      coorY <= config.brdSize - 1 &&
+      cells[coorX][coorY] !== rival
+    ) {
+      isBlock = false;
+    }
+    if (isBlock === false && countSkewDiagonal >= 5) return winCells;
+
+    return null;
+  }
+
+  return (
+    <GameContext.Provider
+      value={{
+        history,
+        stepNumber,
+        turn,
+        gameover,
+        winCells,
+        isWaiting,
+        roomInfo,
+        playerType,
+        handleClick,
+        setRoomId,
+        setPlayerType,
+      }}
+    >
+      {props.children}
+    </GameContext.Provider>
+  );
+};
+
+export default GameProvider;
